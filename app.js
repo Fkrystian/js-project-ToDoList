3a24fb297368f85e42171cb2992a80e5e5da6fe5
Fkrystian <krystianfpa@gmail.com>
2023-05-05 17:35:26 +0200

    F: load DB on Load, Display new Entry on top

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const itemsListContainer = document.querySelector(`.to-do__list`);
 const inputField = document.getElementById(`new__entry`);
 const inputBtn = document.querySelector(`.new__entry-btn`);
 
-const toDoDB = [];  
+const toDoDB = []; 
+
 
 if(!toDoDB){
   const toDoDB = [
@@ -15,18 +16,27 @@ if(!toDoDB){
   ];  
 }
 
+let entryTemplate = `
+<div class="to-do__item" data-id="id">
+<div class="item__check item__function-box-center"><input type="checkbox" name="" id="" class="item__check-checkbox"></div>
+<div class="item__text">Text</div>
+<div class="item__edit item__function-box-center">
+  <button class="item__btn item__edit-btn">
+  <span class="material-symbols-outlined item__fnc-btn-ico item__edit-btn-ico">edit</span>
+</button>
+</div>
+<div class="item__delete item__function-box-center">
+<button class="item__btn item__delete-btn">
+  <span class="material-symbols-outlined item__fnc-btn-ico item__delete-btn-ico">delete</span>
+</button>
+`;
+
 // Functions
 const getNewEntry = function getDataFromInputField(input){
   let newEntryData = input.value;
-
-  return newEntryData;
-}
-
-
-const addNewEntry = function addNewEntryToDB(data){
-  let newId = toDoDB.length + 1;
+  let newId = toDoDB.length;
   let newStatus = 'pending';
-  let newText = data;
+  let newText = newEntryData;
 
   let newEntry = {
     id: newId,
@@ -34,12 +44,71 @@ const addNewEntry = function addNewEntryToDB(data){
     text: newText,
   }  
 
-  toDoDB.push(newEntry);
+  return newEntry;
+}
+
+
+const addNewEntry = function addNewEntryToDB(data){
+  // let newId = toDoDB.length;
+  // let newStatus = 'pending';
+  // let newText = data;
+
+  // let newEntry = {
+  //   id: newId,
+  //   status: newStatus,
+  //   text: newText,
+  // }  
+
+  toDoDB.push(data);
 
   console.log(toDoDB);
 }
 
+const displayDB = function onLoadDisplayDB(db){
+  db.forEach((item) => {
+    entryTemplate = `
+    <div class="to-do__item" data-id="${item.id}">
+    <div class="item__check item__function-box-center"><input type="checkbox" name="" id="" class="item__check-checkbox"></div>
+    <div class="item__text">${item.text}</div>
+    <div class="item__edit item__function-box-center">
+      <button class="item__btn item__edit-btn">
+      <span class="material-symbols-outlined item__fnc-btn-ico item__edit-btn-ico">edit</span>
+    </button>
+    </div>
+    <div class="item__delete item__function-box-center">
+    <button class="item__btn item__delete-btn">
+      <span class="material-symbols-outlined item__fnc-btn-ico item__delete-btn-ico">delete</span>
+    </button>
+    `;
+    
+    itemsListContainer.insertAdjacentHTML('afterbegin', entryTemplate);
+  })
+}
+
+const displayNewEntry = function displayNewEntry(entry){
+  entryTemplate = `
+  <div class="to-do__item" data-id="${entry.id}">
+  <div class="item__check item__function-box-center"><input type="checkbox" name="" id="" class="item__check-checkbox"></div>
+  <div class="item__text">${entry.text}</div>
+  <div class="item__edit item__function-box-center">
+    <button class="item__btn item__edit-btn">
+    <span class="material-symbols-outlined item__fnc-btn-ico item__edit-btn-ico">edit</span>
+  </button>
+  </div>
+  <div class="item__delete item__function-box-center">
+  <button class="item__btn item__delete-btn">
+    <span class="material-symbols-outlined item__fnc-btn-ico item__delete-btn-ico">delete</span>
+  </button>
+  `;
+  
+  itemsListContainer.insertAdjacentHTML('afterbegin', entryTemplate);
+}
+
 // Calls
+displayDB(toDoDB);
+
 inputBtn.addEventListener('click', function() {
   addNewEntry(getNewEntry(inputField));  
+  displayNewEntry(getNewEntry(inputField));
 })
+
